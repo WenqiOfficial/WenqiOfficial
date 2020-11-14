d4ea47ecb1517f26eacfae89e21a33a0cb6c0692
WenqiOfficial <2747297361@qq.com>
2020-11-14 17:33:23 +0800

    Site updated: 2020-11-14 17:33:14

--- game/ff/js/f.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('myButton').addEventListener('click', () => {
-	const script = document.createElement("script");
-	script.src = "https://cdn.jsdelivr.net/gh/wallleap/cdn/js/sakura.js";
-	document.head.appendChild(script);
-})
