cb37f396f297a09a7a2bfe7a4f5e159303ff53fd
WenqiOfficial <2747297361@qq.com>
2020-11-23 02:07:50 +0800

    Site updated: 2020-11-23 02:07:46

---
+++ js/wenqi.js
@@ -0,0 +1,14 @@
+console.log(`
+O))        O))                               
+O))        O))                                 O)
+O))   O)   O))    O))     O)) O))       O))      
+O))  O))   O))  O)   O))  O))  O))    O)  O))  O))
+O)) O) O)) O)) O))))) O)) O))  O))    O)  O))  O))
+O) O)    O)))) O)         O))  O))     O))O))  O))
+O))        O))   O))))    O))  O))        O))  O))
+                                          O)))   
+
+    ©CopyRight Wenqi | 2020 
+	   一切归 闻柒 所有
+
+`);
